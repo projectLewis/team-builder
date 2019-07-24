77939eb57d3305c4d29603629fa7ea10827194af
Seth Lewis <lewiscatenary@gmail.com>
2019-07-24 11:43:54 -0500

    initial scaffolding, form component built

---
+++ team-builder/src/components/Form.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <form>
+      <fieldset>
+        <legend>Form</legend>
+        <label>Add Team Member</label>
+        <input type="text" />
+      </fieldset>
+    </form>
+  );
+};
+
+export default Form;
