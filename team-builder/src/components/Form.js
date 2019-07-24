fe10b18a1cf008fec90faf25a5f7594178fcb50c
Seth Lewis <lewiscatenary@gmail.com>
2019-07-24 12:20:15 -0500

    changed email type to email, completed handleChange functionality

--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,19 +1,44 @@
 import React from "react";
 
-const Form = ({ setTeamMember }) => {
+const Form = ({ setTeamMember, teamMember }) => {
   const handleSubmit = e => {
     e.preventDefault();
   };
   const handleChange = e => {
     console.log(e.target.value);
+    setTeamMember({ ...teamMember, [e.target.name]: e.target.value });
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <fieldset>
-        <legend>Form</legend>
-        <label>Add Team Member</label>
-        <input type="text" onChange={handleChange} />
+        <legend>Add A Team Member</legend>
+        <label>Name:</label>
+        <br />
+        <input
+          type="text"
+          name="name"
+          placeholder="name"
+          onChange={handleChange}
+        />
+        <br />
+        <label>Email:</label>
+        <br />
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          onChange={handleChange}
+        />
+        <br />
+        <label>Role:</label>
+        <br />
+        <input
+          type="text"
+          name="role"
+          placeholder="team members role"
+          onChange={handleChange}
+        />
       </fieldset>
     </form>
   );
